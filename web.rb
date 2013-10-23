0185ec5c141469e5b5277336e61a62e747e3c9b0
raa0121 <raa0121@gmail.com>
2013-10-23 23:45:21 +0900

    bot_verifier

--- web.rb
+++ web.rb
@@ -8,5 +8,5 @@ post '/:room' do
   repo = travis['payload']['repository']
   status = travis['payload']['status_message']
   compare = travis['payload']['compare_url']
-  open("http://lingr.com/api/room/say?#{params[:room]}&bot_id=travis_ci&text=#{repo['owner_name']}/#{repo['name']} #{status}\n#{compare}").read
+  open("http://lingr.com/api/room/say?#{params[:room]}&bot_id=travis_ci&text=[#{repo['owner_name']}/#{repo['name']}]#{status}\n#{compare}&bot_verifier=7658a4358577b8160d7ac05037e47034515e71e8").read
 end
