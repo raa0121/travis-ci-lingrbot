08f79cf6f924c30e40160b04c363ff1e5478abe3
raa0121 <raa0121@gmail.com>
2013-10-24 00:55:29 +0900

    add commit and travis-url

--- web.rb
+++ web.rb
@@ -8,5 +8,6 @@ post '/:room' do
   repo = travis['payload']['repository']
   status = travis['payload']['status_message']
   compare = travis['payload']['compare_url']
-  open("http://lingr.com/api/room/say?room=#{params[:room]}&bot=travis_ci&text=[#{repo['owner_name']}/#{repo['name']}]#{status}\n#{compare}&bot_verifier=255c91a32fc7e70b3421129ad0251df6c2c897d4").read
+  commit = travis['payload']['message']
+  open("http://lingr.com/api/room/say?room=#{params[:room]}&bot=travis_ci&text=[#{repo['owner_name']}/#{repo['name']}]#{status}:#{commit}\n#{compare}\nhttps://travis-ci.org/#{repo['owner_name']}/#{repo['name']}/builds&bot_verifier=255c91a32fc7e70b3421129ad0251df6c2c897d4").read
 end
