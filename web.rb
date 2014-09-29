ba49b0ffb45300cca564d5e959621a8aa33fa0f3
raa0121 <raa0121@gmail.com>
2014-09-29 18:15:05 +0900

    fix

--- web.rb
+++ web.rb
@@ -18,10 +18,10 @@ end
 
 post '/:room' do
   content_type :text
-  travis = JSON.parse(request.body.read.sub("payload=",""))
-  repo = travis['payload']['repository']
-  status = travis['payload']['status_message']
-  compare = travis['payload']['compare_url']
-  commit = travis['payload']['message']
+  travis = JSON.parse(prams[:payload])
+  repo = travis['repository']
+  status = travis['status_message']
+  compare = travis['compare_url']
+  commit = travis['message']
   open("http://lingr.com/api/room/say?room=#{params[:room]}&bot=travis_ci&text=[#{repo['owner_name']}/#{repo['name']}]#{status}:#{commit}\n#{compare}\nhttps://travis-ci.org/#{repo['owner_name']}/#{repo['name']}/builds&bot_verifier=255c91a32fc7e70b3421129ad0251df6c2c897d4").read
 end
