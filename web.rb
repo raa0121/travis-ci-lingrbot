bac12dc4d4f1be0d606c9df6c681d3b811e65c28
raa0121 <raa0121@gmail.com>
2015-01-07 23:21:30 +0900

    fix

--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'json'
 require 'open-uri'
+require 'cgi'
 
 get '/' do
   content_type :text
@@ -23,5 +24,6 @@ post '/:room' do
   status = travis['status_message']
   compare = travis['compare_url']
   commit = travis['message']
-  open("http://lingr.com/api/room/say?room=#{params[:room]}&bot=travis_ci&text=[#{repo['owner_name']}/#{repo['name']}]#{status}:#{commit}\n#{compare}\nhttps://travis-ci.org/#{repo['owner_name']}/#{repo['name']}/builds&bot_verifier=255c91a32fc7e70b3421129ad0251df6c2c897d4").read
+  text = "[#{repo['owner_name']}/#{repo['name']}]#{status}:#{commit}\n#{compare}\nhttps://travis-ci.org/#{repo['owner_name']}/#{repo['name']}/builds"
+  open("http://lingr.com/api/room/say?room=#{params[:room]}&bot=travis_ci&text=#{text}&bot_verifier=255c91a32fc7e70b3421129ad0251df6c2c897d4").read
 end
